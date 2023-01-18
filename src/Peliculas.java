2c250d11df4e5ed95b43cb028ad02b38a946a026
adrrc03 <105524000+adrrc03@users.noreply.github.com>
2023-01-18 20:31:17 +0100

    Constructor adapatado al lector

--- src/Peliculas.java
+++ src/Peliculas.java
@@ -5,12 +5,16 @@ public class Peliculas {
     private int sala;
     private double precio;
 
-    public Peliculas(String nombre, int rating, int sala, double precio, boolean visualizado){
-        this.nombre=nombre;
-        this.rating=rating;
-        this.precio=precio;
-        this.sala=sala;
-        this.visualizado=visualizado;
+    public Peliculas(String[] parametros){
+        this.nombre=parametros[0];
+        this.rating=Integer.parseInt(parametros[1]);
+        this.precio=Double.parseDouble(parametros[2]);
+        this.sala=Integer.parseInt(parametros[3]);
+        if (Integer.parseInt(parametros[4]) == 1){
+            this.visualizado=true;
+        }else{
+            this.visualizado=false;
+        }
     }
 
     public String getNombre() {
