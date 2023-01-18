1243565150832c2bc88977079507c33e4bc5da20
adrrc03 <105524000+adrrc03@users.noreply.github.com>
2023-01-18 19:45:35 +0100

    Creado constructor de Peliculas

--- src/Peliculas.java
+++ src/Peliculas.java
@@ -1,2 +1,15 @@
 public class Peliculas {
+    private String nombre;
+    private boolean visualizado;
+    private int rating;
+    private int sala;
+    private double precio;
+
+    public Peliculas(String nombre, int rating, int sala, double precio, boolean visualizado){
+        this.nombre=nombre;
+        this.rating=rating;
+        this.precio=precio;
+        this.sala=sala;
+        this.visualizado=visualizado;
+    }
 }
