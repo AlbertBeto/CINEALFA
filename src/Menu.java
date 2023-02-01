ce54c0e24db39a9b425c71ac547ef642581fb607
adrrc03 <a.ruiz.cayuelas@gmail.com>
2023-02-01 18:25:53 +0100

    Solved issues with menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Menu {
-    public static void main(String[] args) {
+    public static void cartelera() {
         Pelicula[] peliculas = LectorExterno.lector();
         Scanner sc = new Scanner(System.in);
 
